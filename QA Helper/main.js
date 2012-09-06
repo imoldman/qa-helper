fa58489aace366c69c55324b83c4bd16accebb23
gengxin <melodygeng@126.com>
2012-09-06 20:37:20 +0800

    在选择过滤条件以及翻页后恢复颜色标签。

--- QA Helper/main.js
+++ QA Helper/main.js
@@ -1,45 +1,71 @@
-$(function(){
-    //定义变量
-    var _tds = $('#watcherlist td:first-child');
-    var _data;
-    
-    //绑定事件
-    _tds.click(
-        function(){
+$(function() {
+
+    // 执行数据加载
+    loadLocalData();
+
+    /**
+     * 加载原有数据
+     */
+    function loadLocalData() {
+        // 定义变量
+        var tds, // 表格中的每一行的第一个单元格
+        forms, // 所有的表单
+        pagerLinks, // 翻页链接
+        data; // 数据记录
+
+        // 加载数据
+        if (localStorage.data) {
+            data = JSON.parse(localStorage.data);
+        } else {
+            data = {};
+        }
+
+        // 恢复每一行的颜色设置
+        tds = $('#watcherlist td:first-child');
+        tds.each(function() {
+            var _id = $(this).next().text();
+            if (data[_id]) {
+                $(this).addClass(data[_id]);
+            } else {
+                $(this).addClass('w-flag-0');
+            }
+        });
+
+        // 绑定每一行的点击事件
+        tds.click(function() {
             var _r = /w-flag-(\d)/;
             var _match = this.className.match(_r);
             var _class = _match[0];
             var _num = parseInt(_match[1]);
-
-            //更新样式
+            // 更新样式
             $(this).removeClass(_class);
-            _num = (_num === 3)? 0 : (_num + 1);
+            _num = (_num === 3) ? 0 : (_num + 1);
             var _newClass = 'w-flag-' + _num;
             $(this).addClass(_newClass);
-
-            //更新数据
+            // 更新数据
             var _id = $(this).next().text();
-            _data[_id] = _newClass;
+            data[_id] = _newClass;
+            // 存储数据
+            localStorage.data = JSON.stringify(data);
+        });
 
-            //存储数据
-            localStorage.data = JSON.stringify(_data);
-        }
-    );
-    
-    //加载原有数据
-    if (localStorage.data) {
-        _data = JSON.parse(localStorage.data);
-    } else {
-        _data = {};
+        // 绑定form提交的事件
+        forms = $('form');
+        forms.submit(function() {
+            delayedLoad();
+        });
+
+        // 绑定翻页事件
+        pagerLinks = $('#content .pagination>a');
+        pagerLinks.click(function() {
+            delayedLoad();
+        });
     }
-    _tds.each(
-        function() {
-            var _id = $(this).next().text();
-            if (_data[_id]) {
-                $(this).addClass(_data[_id]);
-            } else {
-                $(this).addClass('w-flag-0');
-            }
-        }
-    );
+
+    /**
+     * 延迟加载
+     */
+    function delayedLoad() {
+        setTimeout(loadLocalData, 2000);
+    }
 });
